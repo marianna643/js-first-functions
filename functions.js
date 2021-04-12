3f8f4e415f5d22c4a48e00a273e2a22830ca23cc
marianna643 <trma0520@gmail.com>
2021-04-12 19:14:42 +0200

    functions solution

--- functions.js
+++ functions.js
@@ -13,21 +13,34 @@ console.log(greeting);
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
-
-
+function getSchool(){
+  return "Novi Hogeschool";
+}
+console.log (getSchool());
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
-
-
+function compliment(){
+  return "Lekker bezig met die functies, Marianna!";
+}
+const outcome = compliment();
+console.log(outcome);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
-
-
+function getGrades (){
+  const cijfer = [8, 8.5, 6, 7];
+  return cijfer;
+}
+console.log(getGrades());
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+function getDetails () {
+  return { firstName: 'Jan', lastName: 'Janssen' };
+}
+
+console.log(getDetails());
